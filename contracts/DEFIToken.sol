a84fe09171557e45d2a5ff0424fb50dac8fc2665
Remix Workspace <>
2022-07-28 20:18:20 -0300

    Transfer Functions

--- contracts/DEFIToken.sol
+++ contracts/DEFIToken.sol
@@ -30,4 +30,32 @@ contract DefiToken {
         balanceOf[msg.sender] = totalSupply;
     }
 
+    // Tokens transfer from an user
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value, "Insuficient Balance");
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    // Allowance function
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    // Tokens transfer from an specific user
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[_from] >= _value, "Insuficient Balance");
+        require(allowance[_from][msg.sender] >= _value, "Insuficient Balance");
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        allowance[_from][msg.sender] -= _value;
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+
+
 }
